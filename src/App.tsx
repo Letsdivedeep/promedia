097a1bf3fdc202f65665b5263980016d5fea97d1
Sakib Rahman <seu.sakib@gmail.com>
2022-07-20 18:20:07 +0600

    Add Routes and public routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import { publicRoutes } from "./pages/Routes/publicRoutes";
 
 function App() {
-  // return <Login />;
-  return <Signup />;
+  return (
+    <Routes>
+      {/* public routes */}
+      {publicRoutes.map(({ path, Component }, index) => (
+        <Route key={index} path={path} element={<Component />} />
+      ))}
+    </Routes>
+  );
 }
 
 export default App;
